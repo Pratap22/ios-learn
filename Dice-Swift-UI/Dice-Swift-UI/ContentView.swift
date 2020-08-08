1407b0bf2d52642c6d4da51384615093ad5fdcd2
Pratap <sharma.pratap22@gmail.com>
2020-08-08 12:51:34 +0530

    completed dice-swift ui

---
+++ Dice-Swift-UI/Dice-Swift-UI/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  Dice-Swift-UI
+//
+//  Created by Pratap Sharma on 08/08/20.
+//  Copyright © 2020 Pratap Sharma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    var body: some View {
+        
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber  = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }.background(Color.red).padding()
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
